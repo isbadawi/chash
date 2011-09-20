de62cf99c6e6c23513a011711deb0487e0269cfd
Ismail Badawi <isbadawi@gmail.com>
2011-09-19 23:04:17 -0400

    Some hackery to have tests automatically run, plus set_up and tear_down functions

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@ hashtable.o: hashtable.c hashtable.h
 	gcc -c hashtable.c
 
 test: hashtable.o minunit.h hashtable_tests.c
-	@gcc hashtable_tests.c hashtable.o -o tests
+	@gcc -c hashtable_tests.c
+	@nm hashtable_tests.o | grep test_ | cut -d " " -f 3 | \
+	 python generate_test_suite.py > real_hashtable_tests.c
+	@gcc real_hashtable_tests.c hashtable.o -o tests
 	@./tests
-	@rm tests
+	@rm hashtable_tests.o real_hashtable_tests.c tests
--- hashtable_tests.c
+++ hashtable_tests.c
@@ -4,76 +4,57 @@
 
 int tests_run = 0;
 
+hash_table* table;
+
+void set_up(void)
+{
+    table = new_hash_table();
+}
+
+void tear_down(void)
+{
+    free_hash_table(table);
+}
+
 char* test_new_hash_table(void)
 {
-    hash_table* table = new_hash_table();
     mu_assert("new_hash_table returns NULL", table != NULL);
-    free_hash_table(table);
     return 0;
 }
 
 char* test_get_missing_key(void)
 {
-    hash_table* table = new_hash_table();
     void *data = get(table, "nosuchkey");
     mu_assert("table returns non-NULL data for missing key", data == NULL);
-    free_hash_table(table);
     return 0;
 }
 
 char* test_get_existing_key(void)
 {
-    hash_table* table = new_hash_table();
     int x = 4;
     put(table, "x", &x);
     int data = GET_AS(int, table, "x");
     mu_assert("table returns wrong data for key", data == 4);
-    free_hash_table(table);
     return 0;
 }
 
 char* test_put_same_key_twice(void)
 {
-    hash_table* table = new_hash_table();
     int x1 = 4;
     int x2 = 5;
     put(table, "x", &x1);
     put(table, "x", &x2);
     int data = GET_AS(int, table, "x");
     mu_assert("table returns wrong data for duplicate key", data == 5);
-    free_hash_table(table);
     return 0;
 }
 
 char* test_delete_key(void)
 {
-    hash_table* table = new_hash_table();
     int x = 4;
     put(table, "x", &x);
     del(table, "x");
     void* data = get(table, "x");
     mu_assert("table return non-NULL for deleted key", data == NULL);
-    free_hash_table(table);
-    return 0;
-}
-
-char* run_tests(void)
-{
-    mu_run_test(test_new_hash_table);
-    mu_run_test(test_get_missing_key);
-    mu_run_test(test_get_existing_key);
-    mu_run_test(test_put_same_key_twice);
-    mu_run_test(test_delete_key);
-    return 0;
-}
-
-int main(int argc, const char *argv[])
-{
-    char* result = run_tests();
-    if (result)
-        printf("%s\n", result);
-    else
-        printf("ALL TESTS PASSED\n");
-    printf("Tests run: %d\n", tests_run);
     return 0;
 }
